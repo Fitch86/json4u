9fddbca8482f9466700baf39b866506089709145
fitch86 <128111344+Fitch86@users.noreply.github.com>
2026-06-23 09:12:17 +0800

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import HeroTitle from "@/containers/landing/HeroTitle";
 import { Pricing } from "@/containers/pricing";
 
 export default function Index() {
-  //redirect("/editor");  
+  redirect("/editor");  
   
   return (
     <div className="relative flex flex-col items-center w-full h-full md:mx-32 mx-4 mb-48 space-y-24 text-center">
