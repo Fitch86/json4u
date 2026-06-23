3ac895ff4e0925ff2b506c73c41adb45be670e2e
fitch86 <128111344+Fitch86@users.noreply.github.com>
2026-06-23 09:03:21 +0800

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,14 +5,13 @@ import Features from "@/containers/landing/Features";
 import HeroTitle from "@/containers/landing/HeroTitle";
 import { Pricing } from "@/containers/pricing";
 
-export default function Index() {  
+export default function Index() {
   const router = useRouter();  
     
   useEffect(() => {  
     router.push("/editor");  
   }, [router]);  
-
-export default function Index() {
+  
   return (
     <div className="relative flex flex-col items-center w-full h-full md:mx-32 mx-4 mb-48 space-y-24 text-center">
       <HeroTitle />
